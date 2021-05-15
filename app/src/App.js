94562da19045a026038c3547874ef043308f2e4c
Joentze <Joentze01@gmail.com>
2021-05-15 07:07:01 -0700

    init, subject card 1st

---
+++ app/src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+
+function App(props) {
+  let iconSubList ={
+    "economics":["economicsCard","econsButton"],
+    "physics":["physicsCard","physicsButton"],
+    "chemistry":["chemistryCard","chemistryButton"]
+  }
+
+  let str1  = `blurCover ${iconSubList[props.subject][0]}`
+  let str2 = `cardButton ${iconSubList[props.subject][1]}`
+  return (
+
+        <div class = "cardMain">
+            <div class = {str1}>
+            <div class="cardInnerText">
+                <div class="sub-icon-econs"></div>
+                <h2>{props.toolName}</h2>
+                <div class="manageSubText">
+                <p>{props.description}</p>
+                    <a href="https://limse10.github.io/econsgraphsweb/"><button class={str2}><i class = "fa fa-play"></i></button></a>
+            </div>
+            
+        </div>
+    </div>
+        </div>
+    
+  
+
+  );
+}
+
+export default App;
