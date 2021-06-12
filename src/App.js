63d120037899d20a988243c9f427611ae80a8114
limse10 <lse.01@hotmail.com>
2021-06-13 00:10:37 +0800

    added page titles and notfound page

--- src/App.js
+++ src/App.js
@@ -1,33 +1,47 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ReactTitle } from "react-meta-tags";
 import ToolsPage from "./components/toolPage";
 import { HomePage } from "./components/HomePage";
 import { AboutPage } from "./components/AboutPage";
 import { FeedbackPage } from "./components/FeedbackPage";
-
+import { NotFoundPage } from "./components/NotFoundPage";
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/">
+          <ReactTitle title="Quark - Home" />
+          <HomePage />
+        </Route>
+        <Route exact path="/t">
+          <ReactTitle title="Quark - Tools" />
           <HomePage />
         </Route>
         <Route path="/about">
+          <ReactTitle title="Quark - About" />
           <AboutPage />
         </Route>
         <Route path="/feedback">
+          <ReactTitle title="Quark - Feedback" />
           <FeedbackPage />
         </Route>
-        <Route path="/ChemBuild">
+        <Route path="/t/chembuild">
+          <ReactTitle title="Quark - ChemBuild" />
           <ToolsPage
             subj="chemistry"
             ToolURL="https://joentze.github.io/ChemicalStructureConstructor/ChemicalStructureConstructor/index.html"
           ></ToolsPage>
         </Route>
-        <Route path="/EconsBuild">
+        <Route path="/t/econsbuild">
+          <ReactTitle title="Quark - EconsBuild" />
           <ToolsPage
             subj="economics"
             ToolURL="https://joentze.github.io/econsgraphs-redesign/econsgraphsweb/index.html"
           ></ToolsPage>
+        </Route>
+        <Route path="*">
+          <ReactTitle title="Quark - Not Found" />
+          <NotFoundPage />
         </Route>
       </Switch>
     </Router>
