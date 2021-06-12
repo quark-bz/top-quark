0ba86b56b36f9fb176c8384452e12585f4039495
limse10 <lse.01@hotmail.com>
2021-06-12 22:32:58 +0800

    thanked ppl for feedback

--- src/components/FeedbackPage.js
+++ src/components/FeedbackPage.js
@@ -1,4 +1,5 @@
-import * as React from "react";
+import React, { useState } from "react";
+
 import Container from "./HomeContainer";
 import Header from "./Header.js";
 import "../assets/avocadoPastel.jpg";
@@ -8,13 +9,16 @@ import Button from "@material-ui/core/Button";
 import db from "../firebase";
 
 export const FeedbackPage = () => {
+  const [sent, setSent] = useState(false);
   const {
     register,
     handleSubmit,
     watch,
     formState: { errors },
   } = useForm();
+
   console.log(watch("message"), errors);
+
   const onSubmit = (data) => {
     db.collection("feedback")
       .add({
@@ -24,6 +28,7 @@ export const FeedbackPage = () => {
       })
       .then(() => {
         console.log("feedback sent!");
+        setSent(true);
       })
       .catch((error) => {
         console.error("error sending feedback: ", error);
@@ -35,45 +40,49 @@ export const FeedbackPage = () => {
       <Header />
       <div id="aboutBackgroundStyling"></div>
       <Container id="feedbackPageContainer">
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <TextField
-            id="name"
-            label="Name"
-            autoFocus
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            {...register("name")}
-          />
-          <TextField
-            id="email"
-            label="Email Address"
-            autoComplete="email"
-            type="email"
-            autoFocus
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            {...register("email")}
-          />
-          <TextField
-            id="message"
-            label="Message"
-            autoFocus
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            multiline
-            rows="10"
-            {...register("message")}
-          />
-          <Button type="submit" fullwidth variant="contained" color="primary">
-            Send Feedback
-          </Button>
-        </form>
+        {sent ? (
+          <div>Thanks for your feedback!</div>
+        ) : (
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <TextField
+              id="name"
+              label="Name"
+              autoFocus
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              {...register("name")}
+            />
+            <TextField
+              id="email"
+              label="Email Address"
+              autoComplete="email"
+              type="email"
+              autoFocus
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              {...register("email")}
+            />
+            <TextField
+              id="message"
+              label="Message"
+              autoFocus
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              multiline
+              rows="10"
+              {...register("message")}
+            />
+            <Button type="submit" fullwidth variant="contained" color="primary">
+              Send Feedback
+            </Button>
+          </form>
+        )}
       </Container>
     </>
   );
